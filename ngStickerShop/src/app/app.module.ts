e5cd4c51c16e287369205e9ecbc802456b91f191
eliotdoesprogramming <zubdoob@gmail.com>
2021-03-05 19:25:23 -0600

    initial commit, framing

---
+++ ngStickerShop/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainMenuComponent } from './navigation/main-menu/main-menu.component';
+import { FootMenuComponent } from './navigation/foot-menu/foot-menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainMenuComponent,
+    FootMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
