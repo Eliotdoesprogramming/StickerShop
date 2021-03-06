301f0980ccb33a8a05a14466d925e5d6963cc8cd
eliotdoesprogramming <zubdoob@gmail.com>
2021-03-05 19:55:02 -0600

    component framework

--- ngStickerShop/src/app/app.module.ts
+++ ngStickerShop/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainMenuComponent } from './navigation/main-menu/main-menu.component';
 import { FootMenuComponent } from './navigation/foot-menu/foot-menu.component';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import { ProductPreviewComponent } from './components/product-preview/product-preview.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainMenuComponent,
-    FootMenuComponent
+    FootMenuComponent,
+    ProductPreviewComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatListModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
