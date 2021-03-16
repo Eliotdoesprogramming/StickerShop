e098ef9aa4a57b2d0cdc0bce3722dff85cf45193
eliotdoesprogramming <zubdoob@gmail.com>
2021-03-15 22:21:33 -0500

    integrated paypal's multiple item purchase request

--- ngStickerShop/src/app/pages/checkout-page/checkout-page.component.ts
+++ ngStickerShop/src/app/pages/checkout-page/checkout-page.component.ts
@@ -10,50 +10,9 @@ import { ProductsService } from 'src/app/services/products.service';
   styleUrls: ['./checkout-page.component.css']
 })
 export class CheckoutPageComponent implements OnInit {
-  
+  cart : cartItem[];
   constructor(private cartService:CartService, private productService: ProductsService) { }
-  // order: OrderRequest = {
-  //   intent: 'CAPTURE', 
-  //   payer: {
-  //     name: {
-  //       given_name: "PayPal",
-  //       surname: "Customer"
-  //     },
-  //     address: {
-  //       address_line_1: '345sd',
-  //       address_line_2: 'Apt 2',
-  //       admin_area_2: 'San Jose',
-  //       admin_area_1: 'CA',
-  //       postal_code: '95121',
-  //       country_code: 'US'
-  //     },
-  //     email_address: "customer@domain.com",
-  //     phone: {
-  //       phone_type: "MOBILE",
-  //       phone_number: {
-  //         national_number: "14082508100"
-  //       }
-  //     }
-  //   },
-  //   purchase_units: [{
-  //     custom_id: 'stickers',
-  //     amount: {
-  //       currency_code: 'USD',
-  //       value: '99.99'
-  //     },
-  //     shipping: {
-  //       address: {
-  //         address_line_1: '',
-  //         address_line_2: 'Building 17',
-  //         admin_area_2: 'San Jose',
-  //         admin_area_1: 'CA',
-  //         postal_code: '95131',
-  //         country_code: 'US'
-  //       }
-  //     }
-  //   }
-  // ]
-  // };
+
   order: OrderRequest = {
     intent: 'CAPTURE', 
     payer: {
@@ -79,39 +38,45 @@ export class CheckoutPageComponent implements OnInit {
     },
     purchase_units: [
       {
-        amount: {
-          breakdown:{
-            item_total:{
-              currency_code:'USD',
-              value:'5.99'
-            },
-            shipping:{
-              currency_code:'USD',
-              value:'1.00'
-            }
+          amount: {
+              currency_code: "USD",
+              value: "200.00",
+              breakdown: {
+                  item_total: {
+                      currency_code: "USD",
+                      value: "200.00"
+                  }
+              }
           },
-          currency_code:'USD',
-          value:'6.99'
-        }
+          items: [
+
+          ],
+
       }
   ]
   };
-  // export interface PurchaseUnitRequest {
-  //   amount: AmountWithBreakdown;
-  //   reference_id?: string;
-  //   payee?: Payee;
-  //   payment_instruction?: PaymentInstruction;
-  //   description?: string;
-  //   custom_id?: string;
-  //   invoice_id?: string;
-  //   soft_descriptor?: string;
-  //   items?: Item[];
-  //   shipping?: Shipping;
-  // }
+
   ngOnInit(): void {
-    let cart: cartItem[] = this.cartService.getCart();
+    this.cart= this.cartService.getCart();
+    this.setSubtotal();
+  }
+  setSubtotal():void {
     let subtotal :number = 0;
-    cart.forEach((item)=> subtotal+=(this.productService.getProduct(item.itemId).price)*item.itemQuantity);
+    this.cart.forEach((item)=> {
+                                  subtotal+=(this.productService.getProduct(item.itemId).price)*item.itemQuantity;
+                                  this.order.purchase_units[0].items.push({
+                                    name:this.productService.getProduct(item.itemId).name,
+                                    description:'test',
+                                    unit_amount:{
+                                      currency_code:'USD',
+                                      value:this.productService.getProduct(item.itemId).price+'',
+                                    },
+                                    quantity:item.itemQuantity+''
+                                  })
+    });
+    this.order.purchase_units[0].amount.value=subtotal+'';
+    this.order.purchase_units[0].amount.breakdown.item_total.value=subtotal+'';
+   console.log(this.order); 
   }
 
 }
